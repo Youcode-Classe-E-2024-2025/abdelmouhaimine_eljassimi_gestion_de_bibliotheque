3730d5ede0becf88de8fb5cb30e430a721ed25b2
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-02-20 23:26:19 +0100

    handle the middleware when the user try to acces to the page without logged in

--- gestion-de-bibliotheque/routes/web.php
+++ gestion-de-bibliotheque/routes/web.php
@@ -5,21 +5,21 @@ use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\ReservationController;
 
+Route::middleware(['auth'])->group(function () {
 
-Route::get('admin', [BookController::class, 'admin']);
-Route::get('/', [BookController::class, 'index']);
-Route::post('DeleteBook', [BookController::class, 'Delete']);
-Route::post('EditBook', [BookController::class, 'Edit']);
+    Route::get('admin', [BookController::class, 'admin']);
+    Route::get('/', [BookController::class, 'index']);
+    Route::post('DeleteBook', [BookController::class, 'Delete']);
+    Route::post('EditBook', [BookController::class, 'Edit']);
+    Route::post('CreateBook', [BookController::class, 'Create']);
 
-Route::get('login', [UserController::class, 'showLoginForm']);
+    Route::get('/reserver/{id}', [ReservationController::class, 'reserve']);
+    Route::get('/return/{id}', [ReservationController::class, 'cancelReservation']);
+});
+
+
+Route::get('login', [UserController::class, 'showLoginForm'])->name('login');
 Route::post('login', [UserController::class, 'login']);
 Route::get('register', [UserController::class, 'showRegistrationForm']);
 Route::post('register', [UserController::class, 'register']);
 Route::get('logout', [UserController::class, 'logout']);
-
-
-Route::post('CreateBook', [BookController::class, 'Create']);
-
-
-Route::get('/reserver/{id}', [ReservationController::class, 'reserve']);
-Route::get('/return/{id}', [ReservationController::class, 'cancelReservation']);
